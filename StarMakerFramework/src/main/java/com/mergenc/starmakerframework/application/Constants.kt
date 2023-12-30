250c08c8fe1ea1cb74d481e24b912ac1901511d4
Mehmet Emin Ergenc <mergencdev@gmail.com>
2023-12-30 19:17:45 +0300

    dev: add Constants classes for constants; dev: add internet permission (I forgot :smw:);

---
+++ StarMakerFramework/src/main/java/com/mergenc/starmakerframework/application/Constants.kt
@@ -0,0 +1,9 @@
+package com.mergenc.starmakerframework.application
+
+/**
+ * Created by Mehmet Emin Ergenc on 12/30/2023
+ */
+object Constants {
+    const val SHARED_PREFS_NAME = "StarPrefs"
+    const val SHARED_PREFS_STARS_LIST = "StarsList"
+}
